35f458d8ae200c3407bf0932c5ab7f7f48ccc7d5
PK <dcspkv@nus.edu.sg>
2024-01-23 16:54:52 +0800

    Coded JS number guessing game

--- it5007.js
+++ it5007.js
@@ -8,3 +8,35 @@ function newsfetch()
         td.innerHTML = newsitems[i]; 
     }
 }
+
+function loadScript(){
+    //Decide on the secret first
+    let secretNumber = Math.floor(Math.random() * 100) + 1;
+    const guessedValue = document.querySelector(".guessField");
+    const guessSubmit = document.querySelector(".guessSubmit");
+    const result = document.querySelector(".result");
+    const lowOrHi = document.querySelector(".lowOrHi");
+    guessSubmit.addEventListener('click', checkguess); //Alternative is to use the onClick option in HTML file
+    function checkguess()
+    {   let guessedValueinNumber = Number(guessedValue.value);
+
+        if (guessedValueinNumber == secretNumber)
+        {
+            result.textContent="Correct Guess!";
+            result.style.backgroundColor = 'green';
+        }
+        else
+        {
+            result.textContent="Incorrect!";
+            result.style.backgroundColor = 'red';
+            if(guessedValueinNumber < secretNumber)
+            {
+                lowOrHi.textContent = "Guess higher!";
+            }
+            else
+            {
+                lowOrHi.textContent = "Guess Lower!";
+            }
+        }
+    }
+}
